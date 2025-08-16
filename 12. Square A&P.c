969f82b25ba23019760e7db73cba2f0a3b0cf159
jugalporia <poriajugal@gmail.com>
2025-08-16 15:06:29 +0530

    Delete 12. Square A&P.c

--- 12. Square A&P.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-    float s,a,p;
-    printf("Enter side length of the square in metres:\n");
-    scanf("%f",&s);
-    a=s*s;
-    p=4*s;
-    printf("Area of your square is %f sq.m\nPerimeter of your square is %f m",a,p);
-}
