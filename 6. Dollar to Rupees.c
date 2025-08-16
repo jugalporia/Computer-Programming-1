859c979aa6c06a6adb4130e9bc9279e7e12c7fa0
jugalporia <poriajugal@gmail.com>
2025-08-16 15:08:22 +0530

    Delete 6. Dollar to Rupees.c

--- 6. Dollar to Rupees.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- void main(){
- 	int a,b;
- 	printf("Enter dollar amount:\n");
-    /*conversion rate given in the question i.e. 1 dollar = 48 rupees */ 
- 	scanf("%d",&a);
- 	b= a*48;
- 	printf("Rupees conversion for that amount is %d Rs.",b);
- }
