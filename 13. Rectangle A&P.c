db922a414d692d75185f1a7fca67e963f5aeda7c
jugalporia <poriajugal@gmail.com>
2025-08-16 15:06:36 +0530

    Delete 13. Rectangle A&P.c

--- 13. Rectangle A&P.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-    float l,b,a,p;
-    printf("Enter length & breadth of the rectangle in metres:\n");
-    scanf("%f%f",&l,&b);
-    a=l*b;
-    p=2.0*(l+b);
-    printf("Area of your rectangle is %f sq.m\nPerimeter of your rectangle is %f m",a,p);
-}
