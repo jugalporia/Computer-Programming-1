b84bc1ea88bc6891f030611d21969d2c44726f07
jugalporia <poriajugal@gmail.com>
2025-08-16 15:06:15 +0530

    Delete 10. Fahrenheit to Celcius.c

--- 10. Fahrenheit to Celcius.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-float main(){
-    float a,b;
-    printf("enter fahrenheit temperature:\n");
-    scanf("%d",&a);
-    b= (a-32)*(5/9.0);
-    printf("that is %d celcius",b);
-}
