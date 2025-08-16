e38c5076d548ec5a78f782712fc76948c448e675
jugalporia <poriajugal@gmail.com>
2025-08-16 15:06:01 +0530

    Delete 14. Trianglearea.c

--- 14. Trianglearea.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main(){
-    float b,h,a;
-    printf("Enter base and height of the triangle in metres:\n");
-    scanf("%f %f",&b,&h);
-    a=0.5*b*h
-    printf("Area of the circle is %f sq.m\n",a);
-}
