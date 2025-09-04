d9028a565717e878905c0af4425d74e30af58c9a
jugalporia <poriajugal@gmail.com>
2025-09-04 13:45:53 +0530

    Add files via upload

---
+++ separatedigits.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+    int n,i;
+    printf("Enter a number:\t");
+    scanf("%d",&i);
+    printf("Separated digits are:\n");
+    for(;i>0;i=i/10){
+        n=i%10;
+        printf("%d\n",n);
+    }
+}
